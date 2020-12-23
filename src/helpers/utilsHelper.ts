eb313b245a368a69db985ba80d8fd23c0e2bd038
Bar Nuri <barn@ke-la.com>
2020-12-23 14:20:02 +0200

    parse date

--- src/helpers/utilsHelper.ts
+++ src/helpers/utilsHelper.ts
@@ -1,5 +1,30 @@
+try {
+    if (JSON && !(JSON as any).dateParser) {
+        const reISO = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*))(?:Z|(\+|-)([\d|:]*))?$/;
+        const reMsAjax = /^\/Date\((d|-|.*)\)[\/|\\]$/;
+        (JSON as any).dateParser = function (key, value) {
+            try {
+                if (typeof value === 'string') {
+                    var a = reISO.exec(value);
+                    if (a) return new Date(value);
+                    a = reMsAjax.exec(value);
+                    if (a) {
+                        var b = a[1].split(/[-+,.]/);
+                        return new Date(b[0] ? +b[0] : 0 - +b[1]);
+                    }
+                }
+                return value;
+            } catch {
+                return value;
+            }
+        };
+    }
+} catch (e) {
+    console.log(e);
+}
+
 export function cloneHelper<T>(value: T): T {
-    return JSON.parse(JSON.stringify(value));
+    return JSON.parse(JSON.stringify(value, (JSON as any).dateParser));
 }
 
 export function jsonPath(json: any, path: string) {
