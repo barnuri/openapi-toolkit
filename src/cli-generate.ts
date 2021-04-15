ce12cf78770b54091e63ebc166932935c375695d
Bar Nuri <barn@ke-la.com>
2021-04-15 10:20:51 +0300

    reorder

--- src/cli-generate.ts
+++ src/cli-generate.ts
@@ -1,22 +1,22 @@
 #!/usr/bin/env node
-import { GeneratorAbstract, TypescriptAxiosGenerator } from './generators';
-import { getSwaggerJson } from './helpers/generatorHelpers';
-import { OpenApiDocument } from './models';
+import { generate } from './generators';
 
 const [, , ...args] = process.argv;
+
 if (args.length % 2 !== 0) {
     console.error('number of args is invalid');
     process.exit(1);
 }
 const argDict = {
     pathOrUrl: '',
-    generator: 'typescript-axios',
+    generatorName: 'typescript-axios',
     outputPath: '',
 };
+
 for (let index = 0; index < args.length / 2; index++) {
     let argName = (args[index * 2] || '').replace(/-/g, '');
     argName = argName === 'i' ? 'pathOrUrl' : argName;
-    argName = argName === 'g' ? 'generator' : argName;
+    argName = argName === 'g' ? 'generatorName' : argName;
     argName = argName === 'o' ? 'outputPath' : argName;
     argDict[argName] = args[index * 2 + 1];
 }
@@ -30,22 +30,6 @@ if (!argDict.outputPath) {
     process.exit(1);
 }
 
-const generatorGetter = (type: string, swagger: OpenApiDocument): GeneratorAbstract => {
-    if (type === 'typescript-axios') {
-        return new TypescriptAxiosGenerator(swagger, argDict.outputPath);
-    }
-    throw new Error('bad generator name: ' + type);
-};
-
-const generate = async () => {
-    console.log(`get swagger`);
-    const swagger = await getSwaggerJson(argDict.pathOrUrl);
-    console.log(`get swagger successfull`);
-    const generator = generatorGetter(argDict.generator, swagger);
-    console.log(`start ${generator.constructor.name}`);
-    generator.generate();
-};
-
-generate()
+generate(argDict.pathOrUrl, argDict.generatorName, argDict.outputPath)
     .then(() => console.log('done succssfully'))
     .catch(err => console.error(`failed ${err}`));
--- src/generators/index.ts
+++ src/generators/index.ts
@@ -1,2 +1,26 @@
+import { GeneratorAbstract, getSwaggerJson, TypescriptAxiosGenerator } from '..';
+import { OpenApiDocument } from '../models';
+
 export * from './TypescriptAxiosGenerator';
 export * from './GeneratorAbstract';
+
+export async function generate(swaggerPathOrUrl: string, generatorName: string, outputPath: string) {
+    console.log(`get swagger`);
+    const swagger = await getSwaggerJson(swaggerPathOrUrl);
+    console.log(`get swagger successfull`);
+    const generator = generatorGetter(generatorName, swagger, outputPath);
+    console.log(`start ${generator.constructor.name}`);
+    await generator.generate();
+}
+
+export const generatorsNames = ['typescript-axios'];
+
+export function generatorGetter(type: string, swagger: OpenApiDocument, outputPath: string): GeneratorAbstract {
+    if (!generatorsNames.find(x => x.toLowerCase() === type.toLowerCase())) {
+        throw new Error(`bad generator name: '${type}', available names: [${generatorsNames.map(x => `'${x}'`).join(', ')}]`);
+    }
+    if (type === 'typescript-axios') {
+        return new TypescriptAxiosGenerator(swagger, outputPath);
+    }
+    throw new Error('not implemented: ' + type);
+}
