701793e9e855e2714046ba856216314faf111493
Bar Nuri <barn@ke-la.com>
2021-08-01 17:13:06 +0300

    c# method name

--- src/generators/server/CSharpServerGenerator.ts
+++ src/generators/server/CSharpServerGenerator.ts
@@ -24,7 +24,7 @@ export class CSharpServerGenerator extends CSharpClientGenerator {
         writeFileSync(controllerFile, '\n' + this.addNamespace(controllerContent));
     }
     generateControllerMethodContent(controller: string, controllerPath: ApiPath): string {
-        const methodName = capitalize(this.getMethodName(controllerPath));
+        const methodName = capitalize(this.getMethodName(controllerPath)) + 'Async';
         let requestType = controllerPath.body.haveBody ? this.getPropDesc(controllerPath.body.schema) : 'object';
         const responseType = this.getPropDesc(controllerPath.response);
         const bodyParam = controllerPath.body.haveBody ? `[FromBody] ${requestType}${!controllerPath.body.required ? `?` : ''} body, ` : '';
