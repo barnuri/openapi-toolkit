5725efe0425301e1c0f830f02e87b915f2ee5713
Bar Nuri <barn@ke-la.com>
2022-04-11 23:01:23 +0300

    add type

--- src/models/ClientGenerators.ts
+++ src/models/ClientGenerators.ts
@@ -1,6 +1,7 @@
 export enum ClientGenerators {
     TypescriptAxios = 'typescript-axios',
+    TypescriptReactQuery = 'typescript-react-query',
     CSharp = 'c#',
     Python = 'python',
-    Go = 'go'
+    Go = 'go',
 }
